8ca0007c2cc3c22ff3f09c49efabd33127a97e25
strosche <robert.scheike@student.hs-anhalt.de>
2021-05-19 11:10:30 +0200

    added county class

---
+++ src/main/java/com/fuzetea/hibernatespringbootvs/entities/Country.java
@@ -0,0 +1,39 @@
+package com.fuzetea.hibernatespringbootvs.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
+    private int id;
+
+    @Column(name = "country_name")
+    private String countryname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+  
+}
